61c024b29b2b78e58a1d6343147f26655406cf11
Mahbuba-Islam <mahbubaislam7010@gmail.com>
2022-11-01 17:47:29 -0400

    app.js update

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+//routes
+const jobRoute = require('./routes/job.route')
+  const candidateRoute = require('./routes/candidate.route')
+ const hirringManagerRoute = require('./routes/manager.route')
+ const userRoute  = require('./routes/user.route')
+
 
 // rote api
 app.get('/', (req, res) =>{
     res.send('running server successfully')
 })
 
-//routes
-const jobRoute = require('./routes/job.route')
-  const candidateRoute = require('./routes/candidate.route')
- const hirringManagerRoute = require('./routes/manager.route')
- const userRoute  = require('./routes/user.route')
 
 
 app.use('/api/v1/jobs', jobRoute )
