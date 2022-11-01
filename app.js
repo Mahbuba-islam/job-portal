4dfc2989a9866a518766bf3f14d58bfc705e35d5
Mahbuba-Islam <mahbubaislam7010@gmail.com>
2022-11-01 06:37:58 -0400

    update

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+
+//middleware
+
+app.use(cors())
+app.use(express.json())
 
 
-app.use(express.json());
-app.use(cors());
+// rote api
+app.get('/', (req, res) =>{
+    res.send('running server successfully')
+})
 
 
 
-app.get("/", (req, res) => {
-  res.send("Route is working! YaY!");
-});
-
-
-module.exports = app;
+module.exports = app
--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
-const colors = require("colors");
-
-
+const dotenv = require('dotenv').config();
+const colors = require('colors')
 const app = require("./app");
 
-// database connection
-mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
-  console.log("Database connection is successful 🛢" .red.bold);
-});
+//database connect 
+mongoose.connect(process.env.DATABASE_CONNECT).then(()=>{
+   console.log(`Database connection is successful`.green.bold)
+})
+
+const port = process.env.PORT||5000
+
+app.listen(port, ()=>{
+  console.log(`App is running on port ${port}`.yellow.bold)
+})
 
 
 
-// server
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`.yellow.bold);
-});
