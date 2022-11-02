ebd187ca2d550b61450bd9ccd48dd32df57b4953
Mahbuba-Islam <mahbubaislam7010@gmail.com>
2022-11-02 04:56:01 -0400

    candidate update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 //routes
 const jobRoute = require('./routes/job.route')
-  const candidateRoute = require('./routes/candidate.route')
+  // const candidateRoute = require('./routes/candidate.route')
  const hirringManagerRoute = require('./routes/manager.route')
  const userRoute  = require('./routes/user.route')
 
@@ -20,15 +20,37 @@ app.get('/', (req, res) =>{
     res.send('running server')
 })
 
-app.get('/api/v1/jobs', (req, res) =>{
-  res.send('running server success')
+
+const Candidate = require("./models/Candidate");
+
+
+   
+    
+app.get('/api/v1/candidate', async (req, res) =>{
+  try {
+   
+    const result = await Candidate.find({})
+res.status(200).json({
+  status: "success",
+  data: result,
+});
+} 
+catch (error) {
+res.status(400).json({
+  status: "fail",
+  message: "cant get data",
+  error: error.message,
+});
+}
+
+
 })
 
 
 
 app.use('/api/v1/jobs', jobRoute )
 app.use('/api/v1/manager/jobs', hirringManagerRoute)
-app.use('/api/v1/candidate', candidateRoute)
+// app.use('/api/v1/candidate', candidateRoute)
 app.use("/api/v1/user", userRoute);
 
 
